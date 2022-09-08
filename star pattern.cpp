dec9e0ff9245122f7860a0d11243c3533c783eb9
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2022-09-08 12:45:28 +0530

    Add files via upload

---
+++ star pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=4;j++)
+		{
+			if(i==1||i==4||j==1||j==7){
+			cout<<"*";
+		}
+			else{
+			cout<<" ";
+		}
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
