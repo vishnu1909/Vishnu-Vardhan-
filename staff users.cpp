c07c0f410c09021501086e5b3f802628c966b71e
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2022-09-08 13:42:14 +0530

    Add files via upload

---
+++ staff users.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	cout<<"total users:";
+	cin>>b;
+	cout<<"staff users:";
+	cin>>a;
+	c=a/3;
+	d=a+c;
+	e=b-d;
+	{
+	cout<<"student users= "<<e;
+    }
+	return 0;
+}
