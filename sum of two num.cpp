a782e532db5be139e32638a7863538540999a23f
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2022-09-08 12:41:41 +0530

    Add files via upload

---
+++ sum of two num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"enter two numbers:";
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"the sum of two numbers is "<<a<<" + "<<b<<" = "<<sum;
+	return 0;
+}
